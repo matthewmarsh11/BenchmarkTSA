76fcf3332d708ffacab7f421b60948f28699339d
matthewmarsh11 <matthewmarsh2001@icloud.com>
2025-02-21 14:49:18 +0000

    added encoder decoder TF

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
-from base import TrainingConfig, BaseModel, LSTMConfig, CNNConfig, MLPConfig
+from base import TrainingConfig, BaseModel, BaseEncoderDecoder, LSTMConfig, CNNConfig, MLPConfig
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from dataclasses import dataclass
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -229,7 +229,7 @@ class EncoderLSTM(BaseModel):
             for _ in range(self.config.num_layers - 1)
         ])
         
-    def forward(self, x: torch.Tensor, hidden: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, hidden: Optional[Tuple[torch.Tensor, torch.Tensor]] = None) -> torch.Tensor:
         
         num_directions = 2 if self.config.bidirectional else 1
         if hidden is None:
@@ -292,7 +292,7 @@ class DecoderLSTM(BaseModel):
         self.monte_carlo = monte_carlo
         self.var = var
 
-        lstm_output_dim = self.hidden_dim * 2 if self.config.bidirectional else self.hidden_dim
+        lstm_output_dim = self.config.hidden_dim * 2 if self.config.bidirectional else self.config.hidden_dim
 
         self.BatchNorm = nn.BatchNorm1d(lstm_output_dim) if getattr(self.config.norm_type, 'batch', False) else None
         self.LayerNorm = nn.LayerNorm(lstm_output_dim) if getattr(self.config.norm_type, 'layer', False) else None
@@ -426,10 +426,10 @@ class DecoderLSTM(BaseModel):
 
 # Combined Encoder-Decoder Model (Can be used in combination with other models!)
 
-class EncoderDecoder(BaseModel):
+class EncoderDecoder(BaseEncoderDecoder):
     
-    def __init__(self, config: LSTMConfig, encoder_model: BaseModel, decoder_model: BaseModel):
-        super().__init__(config)
+    def __init__(self, encoder_config: LSTMConfig, decoder_config: LSTMConfig, encoder_model: BaseModel, decoder_model: BaseModel):
+        super().__init__(encoder_config, decoder_config)
         self.encoder_model = encoder_model
         self.decoder_model = decoder_model
         
@@ -443,64 +443,6 @@ class EncoderDecoder(BaseModel):
         
         return decoder_output
         
-    
-
-# https://colab.research.google.com/github/PawaritL/BayesianLSTM/blob/master/Energy_Consumption_Predictions_with_Bayesian_LSTMs_in_PyTorch.ipynb#scrollTo=OgWyOffPbO0b
-
-class BayesianLSTM(nn.Module):
-
-    def __init__(self, n_features, output_length, batch_size):
-
-        super(BayesianLSTM, self).__init__()
-
-        self.batch_size = batch_size # user-defined
-
-        self.hidden_size_1 = 128 # number of encoder cells (from paper)
-        self.hidden_size_2 = 32 # number of decoder cells (from paper)
-        self.stacked_layers = 2 # number of (stacked) LSTM layers for each stage
-        self.dropout_probability = 0.5 # arbitrary value (the paper suggests that performance is generally stable across all ranges)
-
-        self.lstm1 = nn.LSTM(n_features, 
-                             self.hidden_size_1, 
-                             num_layers=self.stacked_layers,
-                             batch_first=True)
-        self.lstm2 = nn.LSTM(self.hidden_size_1,
-                             self.hidden_size_2,
-                             num_layers=self.stacked_layers,
-                             batch_first=True)
-        
-        self.fc = nn.Linear(self.hidden_size_2, output_length)
-        self.loss_fn = nn.MSELoss()
-        
-    def forward(self, x):
-        batch_size, seq_len, _ = x.size()
-
-        hidden = self.init_hidden1(batch_size)
-        output, _ = self.lstm1(x, hidden)
-        output = F.dropout(output, p=self.dropout_probability, training=True)
-        state = self.init_hidden2(batch_size)
-        output, state = self.lstm2(output, state)
-        output = F.dropout(output, p=self.dropout_probability, training=True)
-        output = output[:, -1, :] # take the last decoder cell's outputs
-        y_pred = self.fc(output)
-        return y_pred
-        
-    def init_hidden1(self, batch_size):
-        hidden_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_1))
-        cell_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_1))
-        return hidden_state, cell_state
-    
-    def init_hidden2(self, batch_size):
-        hidden_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_2))
-        cell_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_2))
-        return hidden_state, cell_state
-    
-    def loss(self, pred, truth):
-        return self.loss_fn(pred, truth)
-
-    def predict(self, X):
-        return self(torch.tensor(X, dtype=torch.float32)).view(-1).detach().numpy()
-
 # Convolutional Neural Networks (CNNs)
 
 class CNN(BaseModel):
@@ -817,7 +759,7 @@ class TransformerEncoder(BaseModel):
         )
         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers=self.config.num_layers)
         
-        # Output layers
+        # Output layers for using encoder only model
         self.output_layer = nn.Sequential(
             nn.Linear(self.config.d_model, self.config.dim_feedforward),
             nn.ReLU(),
@@ -866,6 +808,185 @@ class TransformerEncoder(BaseModel):
         return output.view(-1, self.horizon, self.output_dim)
 
 # Encoder and Decoder Transformers
+
+class EcDcTransformer(BaseEncoderDecoder):
+    """Encoder-Decoder Transformer model"""
+    def __init__(self, 
+        input_size: int,
+        horizon: int,
+        batch_first: bool,
+        out_seq_len: int=58,
+        dim_val: int=512,  
+        n_encoder_layers: int=4,
+        n_decoder_layers: int=4,
+        n_heads: int=8,
+        dropout_encoder: float=0.2, 
+        dropout_decoder: float=0.2,
+        dropout_pos_enc: float=0.1,
+        dim_feedforward_encoder: int=2048,
+        dim_feedforward_decoder: int=2048,
+        num_predicted_features: int=1
+        ): 
+
+        """
+        Args:
+            input_size: int, number of input variables. 1 if univariate.
+            dec_seq_len: int, the length of the input sequence fed to the decoder
+            dim_val: int, aka d_model. All sub-layers in the model produce 
+                     outputs of dimension dim_val
+            n_encoder_layers: int, number of stacked encoder layers in the encoder
+            n_decoder_layers: int, number of stacked encoder layers in the decoder
+            n_heads: int, the number of attention heads (aka parallel attention layers)
+            dropout_encoder: float, the dropout rate of the encoder
+            dropout_decoder: float, the dropout rate of the decoder
+            dropout_pos_enc: float, the dropout rate of the positional encoder
+            dim_feedforward_encoder: int, number of neurons in the linear layer 
+                                     of the encoder
+            dim_feedforward_decoder: int, number of neurons in the linear layer 
+                                     of the decoder
+            num_predicted_features: int, the number of features you want to predict.
+                                    Most of the time, this will be 1 because we're
+                                    only forecasting FCR-N prices in DK2, but in
+                                    we wanted to also predict FCR-D with the same
+                                    model, num_predicted_features should be 2.
+        """
+
+        super().__init__() 
+
+        self.dec_seq_len = dec_seq_len
+
+        #print("input_size is: {}".format(input_size))
+        #print("dim_val is: {}".format(dim_val))
+
+        # Creating the three linear layers needed for the model
+        self.encoder_input_layer = nn.Linear(
+            in_features=input_size, 
+            out_features=dim_val 
+            )
+
+        self.decoder_input_layer = nn.Linear(
+            in_features=num_predicted_features,
+            out_features=dim_val
+            )  
+        
+        self.linear_mapping = nn.Linear(
+            in_features=dim_val, 
+            out_features=num_predicted_features
+            )
+
+        # Create positional encoder
+        self.positional_encoding_layer = pe.PositionalEncoder(
+            d_model=dim_val,
+            dropout=dropout_pos_enc
+            )
+
+        # The encoder layer used in the paper is identical to the one used by
+        # Vaswani et al (2017) on which the PyTorch module is based.
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=dim_val, 
+            nhead=n_heads,
+            dim_feedforward=dim_feedforward_encoder,
+            dropout=dropout_encoder,
+            batch_first=batch_first
+            )
+
+        # Stack the encoder layers in nn.TransformerDecoder
+        # It seems the option of passing a normalization instance is redundant
+        # in my case, because nn.TransformerEncoderLayer per default normalizes
+        # after each sub-layer
+        # (https://github.com/pytorch/pytorch/issues/24930).
+        self.encoder = nn.TransformerEncoder(
+            encoder_layer=encoder_layer,
+            num_layers=n_encoder_layers, 
+            norm=None
+            )
+
+        decoder_layer = nn.TransformerDecoderLayer(
+            d_model=dim_val,
+            nhead=n_heads,
+            dim_feedforward=dim_feedforward_decoder,
+            dropout=dropout_decoder,
+            batch_first=batch_first
+            )
+
+        # Stack the decoder layers in nn.TransformerDecoder
+        # It seems the option of passing a normalization instance is redundant
+        # in my case, because nn.TransformerDecoderLayer per default normalizes
+        # after each sub-layer
+        # (https://github.com/pytorch/pytorch/issues/24930).
+        self.decoder = nn.TransformerDecoder(
+            decoder_layer=decoder_layer,
+            num_layers=n_decoder_layers, 
+            norm=None
+            )
+
+    def forward(self, src: torch.Tensor, tgt: torch.Tensor, src_mask: torch.Tensor=None, 
+                tgt_mask: torch.Tensor=None) -> torch.Tensor:
+        """
+        Returns a tensor of shape:
+        [target_sequence_length, batch_size, num_predicted_features]
+        
+        Args:
+            src: the encoder's output sequence. Shape: (S,E) for unbatched input, 
+                 (S, N, E) if batch_first=False or (N, S, E) if 
+                 batch_first=True, where S is the source sequence length, 
+                 N is the batch size, and E is the number of features (1 if univariate)
+            tgt: the sequence to the decoder. Shape: (T,E) for unbatched input, 
+                 (T, N, E)(T,N,E) if batch_first=False or (N, T, E) if 
+                 batch_first=True, where T is the target sequence length, 
+                 N is the batch size, and E is the number of features (1 if univariate)
+            src_mask: the mask for the src sequence to prevent the model from 
+                      using data points from the target sequence
+            tgt_mask: the mask for the tgt sequence to prevent the model from
+                      using data points from the target sequence
+        """
+
+        #print("From model.forward(): Size of src as given to forward(): {}".format(src.size()))
+        #print("From model.forward(): tgt size = {}".format(tgt.size()))
+
+        # Pass throguh the input layer right before the encoder
+        src = self.encoder_input_layer(src) # src shape: [batch_size, src length, dim_val] regardless of number of input features
+        #print("From model.forward(): Size of src after input layer: {}".format(src.size()))
+
+        # Pass through the positional encoding layer
+        src = self.positional_encoding_layer(src) # src shape: [batch_size, src length, dim_val] regardless of number of input features
+        #print("From model.forward(): Size of src after pos_enc layer: {}".format(src.size()))
+
+        # Pass through all the stacked encoder layers in the encoder
+        # Masking is only needed in the encoder if input sequences are padded
+        # which they are not in this time series use case, because all my
+        # input sequences are naturally of the same length. 
+        # (https://github.com/huggingface/transformers/issues/4083)
+        src = self.encoder( # src shape: [batch_size, enc_seq_len, dim_val]
+            src=src
+            )
+        #print("From model.forward(): Size of src after encoder: {}".format(src.size()))
+
+        # Pass decoder input through decoder input layer
+        decoder_output = self.decoder_input_layer(tgt) # src shape: [target sequence length, batch_size, dim_val] regardless of number of input features
+        #print("From model.forward(): Size of decoder_output after linear decoder layer: {}".format(decoder_output.size()))
+
+        #if src_mask is not None:
+            #print("From model.forward(): Size of src_mask: {}".format(src_mask.size()))
+        #if tgt_mask is not None:
+            #print("From model.forward(): Size of tgt_mask: {}".format(tgt_mask.size()))
+
+        # Pass throguh decoder - output shape: [batch_size, target seq len, dim_val]
+        decoder_output = self.decoder(
+            tgt=decoder_output,
+            memory=src,
+            tgt_mask=tgt_mask,
+            memory_mask=src_mask
+            )
+
+        #print("From model.forward(): decoder_output shape after decoder: {}".format(decoder_output.shape))
+
+        # Pass through linear mapping
+        decoder_output = self.linear_mapping(decoder_output) # shape [batch_size, target seq len]
+        #print("From model.forward(): decoder_output size after linear_mapping = {}".format(decoder_output.size()))
+
+        return decoder_output
+
 
 # Graph Neural Networks (GNNs)
 
